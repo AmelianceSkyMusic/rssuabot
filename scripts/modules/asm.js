823ece6823077cead6d4f10b7cd4336f9b7dc3de
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-10 17:28:35 +0300

    + add asm main module

---
+++ scripts/modules/asm.js
@@ -0,0 +1,13 @@
+const { getRandomNumber } = require('./getRandomNumber');
+const { secToMs } = require('./secToMs');
+const { minToMs } = require('./minToMs');
+
+// >----------------------------------------------------------------<
+// >                             EXPORT                             <
+// >----------------------------------------------------------------<
+
+module.exports = {
+	getRandomNumber,
+	secToMs,
+	minToMs
+}
