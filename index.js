39972fe5940eb55f406b404572b31e7900448282
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-13 23:53:10 +0300

    * test update

--- index.js
+++ index.js
@@ -548,6 +548,7 @@ bot.on("channel_post", async (ctx) => {
 		} else if(channelUsername === 'DesignIs_Official') { // test
 			const channelPost = ctx.update.channel_post.text
 			log(channelUsername, channelPost)
+			await ctx.telegram.sendMessage(RSSUA_CHAT_ID, channelPost, {parse_mode: 'Markdown'})
 			await ctx.telegram.sendMessage(RSS2022Q1UA_CHAT_ID, channelPost, {parse_mode: 'Markdown'})
 			await ctx.telegram.sendMessage(TEST_CHAT_ID, channelPost, {parse_mode: 'Markdown'})
 		}
