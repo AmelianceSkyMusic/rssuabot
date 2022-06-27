6604191867e071313cd79fe5490acfe40482b12f
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-27 13:11:00 +0300

    + isReply

--- scripts/functions/isReply.js
+++ scripts/functions/isReply.js
@@ -15,6 +15,6 @@ export default function isReply(ctx) {
 	try {
 		return ctx.update.message?.reply_to_message?.from ? true : false;
 	} catch (error) {
-		f.error(isReply, error);
+		f.error('isReply', error);
 	}
 }
