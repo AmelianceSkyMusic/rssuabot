0cf0360349dffd4f1b42cf6b5675bf1e94aa8295
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-06-27 13:11:53 +0300

    * refactor botCommandMute

--- scripts/commands/botCommandMute.js
+++ scripts/commands/botCommandMute.js
@@ -17,62 +17,51 @@ const { log } = console;
 export default function botCommandMute() {
 	APP.BOT.command('mute', async (ctx) => {
 		try {
+			log(ctx.update.message.text.split('@')[0]);
 
 			await f.ctxRemoveCommandMsg(ctx, 0);
-			const from = ctx.update.message.from;
-
-			log(from);
-
-			const replyTo = ctx.update.message?.reply_to_message?.from;
 
 			if (f.isReply(ctx)) {
 
 				const chatId = ctx.update.message.chat.id;
 
-				// *----- action from -----
-				const from = ctx.update.message.from;
+				// *----- from user -----
+				const {
+					userId,
+					userFirstName,
+					userUsername,
+					userMentionHTML,
+					userStatus
+				} = f.getUserInfo(ctx);
 
-				log(from);
-
-				const {	id: fromUserId,
-					first_name: fromUserFirstName,
-					username: fromUserUsername } = from;
-				const userFrom = `<a href="tg://user?id=${fromUserId}">${fromUserFirstName}</a>`;
-				const fromGetChatMember = await ctx.telegram.getChatMember(chatId, fromUserId);
-				const fromMemberStatus = fromGetChatMember.status;
-
-				// *----- reply to user -----
-				const {	id: replyToId,
-					first_name: replyToFirstName,
-					username: replyToUsername } = replyTo;
-				const userReplyTo = `<a href="tg://user?id=${replyToId}">${replyToFirstName}</a>`;
-				const replyToGetChatMember = await ctx.telegram.getChatMember(chatId, replyToId);
-				const replyToStatus = replyToGetChatMember.status;
+				// *----- reply to -----
+				const {
+					replyId,
+					replyFirstName,
+					replyUsername,
+					replyMentionHTML,
+					replyStatus
+				} = f.getUserInfo(ctx);
 
 				// *----- detect if user has access -----
-				if (fromMemberStatus === 'creator' || fromMemberStatus === 'administrator') {
-					if (replyToStatus !== 'creator' && replyToStatus !== 'administrator') {
-						await ctx.telegram.restrictChatMember(chatId, replyToId);
-						const randomMsg = await ctx.replyWithHTML(`${userFrom} зам'ютив ${userReplyTo}`);
-						log(`\n-\n${fromUserFirstName} @${fromUserUsername} ${fromUserId} (${fromMemberStatus})\n→ mute →\n${replyToFirstName} @${replyToUsername} ${replyToId} (${replyToStatus})\n-\n`);
+				if (userStatus === 'creator' || userStatus === 'administrator') {
+					if (replyStatus !== 'creator' && replyStatus !== 'administrator') {
+						await ctx.telegram.restrictChatMember(chatId, replyId);
+						await ctx.replyWithHTML(`${userMentionHTML} зам'ютив ${replyMentionHTML}`);
+						log(`\n-\n${userFirstName} @${userUsername} ${userId} (${userStatus})\n→ mute →\n${replyFirstName} @${replyUsername} ${replyId} (${replyStatus})\n-\n`);
 					} else {
-						const randomMsg = await ctx.replyWithHTML(`${userFrom} спробував зам'ютити ${userReplyTo}, але нічого не вийшло ¯\\_(ツ)_/¯`);
-						log(`\n-\n${fromUserFirstName} @${fromUserUsername} ${fromUserId} (${fromMemberStatus})\n→ tried to mute →\n${replyToFirstName} @${replyToUsername} ${replyToId} (${replyToStatus})\n-\n`);
+						await ctx.replyWithHTML(`${userMentionHTML} спробував зам'ютити ${replyMentionHTML}, але нічого не вийшло ¯\\_(ツ)_/¯`);
+						log(`\n-\n${userFirstName} @${userUsername} ${userId} (${userStatus})\n→ tried to mute →\n${replyFirstName} @${replyUsername} ${replyId} (${replyStatus})\n-\n`);
 					}
 				} else {
-					const randomMsg = await ctx.replyWithHTML(`${userFrom} спробував зам'ютити ${userReplyTo}, але нічого не вийшло ¯\\_(ツ)_/¯`);
-					log(`\n-\n${fromUserFirstName} @${fromUserUsername} ${fromUserId} (${fromMemberStatus})\n→ tried to mute →\n${replyToFirstName} @${replyToUsername} ${replyToId} (${replyToStatus})\n-\n`);
+					await ctx.replyWithHTML(`${userMentionHTML} спробував зам'ютити ${replyMentionHTML}, але нічого не вийшло ¯\\_(ツ)_/¯`);
+					log(`\n-\n${userFirstName} @${userUsername} ${userId} (${userStatus})\n→ tried to mute →\n${replyFirstName} @${replyUsername} ${replyId} (${replyStatus})\n-\n`);
 				}
 			} else {
-				const msg = await ctx.replyWithHTML(`Команда /mute працює тільки як Reply!`);
-				setTimeout( async () => {
-					try {
-						await ctx.deleteMessage(msg.message_id);
-					} catch (error) { console.error('---------\n→ ASM ERR\n↓ ↓ ↓ ↓ ↓\n', error); }
-				}, asm.secToMs(5));
+				f.sendMsgReplyNotFonud(ctx, botCommandMute, 5);
 			}
 		} catch (error) {
-			console.error('---------\n→ ASM ERR\n↓ ↓ ↓ ↓ ↓\n', error);
+			f.error(botCommandMute, error);
 		}
 	});
 }
