1f8b14ab75668dd913a4a916614566e667e59975
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-22 23:34:51 +0300

    # remove }

--- scripts/commands/botCommandAddMsgToNotion.js
+++ scripts/commands/botCommandAddMsgToNotion.js
@@ -18,7 +18,7 @@ export default function botCommandAddMsgToNotion() {
 		const chatId = APP.RSSUA_CHAT_ID;
 		if (ctx.update.message.chat.id === chatId) {
 			const { userText, userFirstName, userUsername, userId, userDateShort  } = await f.getInfo(ctx);
-			const heading = `${userFirstName} (@${userUsername}) [${userId}]}\n${userDateShort}`;
+			const heading = `${userFirstName} (@${userUsername}) [${userId}]\n${userDateShort}`;
 			await notionAPI.writeBlock(userText, heading);
 		}
 	});
