d020ee587b9375aecdafa306b708ab3dd10920cc
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-22 23:32:05 +0300

    * add message

--- scripts/commands/botCommandAddMsgToNotion.js
+++ scripts/commands/botCommandAddMsgToNotion.js
@@ -15,8 +15,11 @@ export default function botCommandAddMsgToNotion() {
 
 	// ^------------------------ remove service removed left message ------------------------
 	APP.BOT.on('message', async (ctx) => {
-		const { userText, userFirstName, userUsername, userId, userDateShort  } = await f.getInfo(ctx);
-		const heading = `${userFirstName} (@${userUsername}) [${userId}]}\n${userDateShort}`;
-		await notionAPI.writeBlock(userText, heading);
+		const chatId = APP.RSSUA_CHAT_ID;
+		if (ctx.update.message.chat.id === chatId) {
+			const { userText, userFirstName, userUsername, userId, userDateShort  } = await f.getInfo(ctx);
+			const heading = `${userFirstName} (@${userUsername}) [${userId}]}\n${userDateShort}`;
+			await notionAPI.writeBlock(userText, heading);
+		}
 	});
 }
