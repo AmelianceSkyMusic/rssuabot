3364c9f84cb3936914850e684bb326c3510e30a2
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-22 22:48:01 +0300

    # fix

--- scripts/commands/botCommandRandomWordsPhrase.js
+++ scripts/commands/botCommandRandomWordsPhrase.js
@@ -19,27 +19,38 @@ export default function botCommandRandomWordsPhrase()  {
 			const commandMessageId = ctx.update.message.message_id;
 			await f.removeMsgById(ctx, commandMessageId, 0);
 
-			const memberPressed = ctx.update.message.from;
-			const memberPressedId = memberPressed.id;
-			const memberPressedfirstName = memberPressed.first_name;
-			const user = `<a href="tg://user?id=${memberPressedId}">${memberPressedfirstName}</a>`;
-			await f.removeMsgById(ctx, commandMessageId, 30);
-			let message = [];
 
-			const countOfWords = asm.getRandomNumber(1, 10);
-			log(APP.notion.rssuabot.text);
-			for (let i = 0; i < countOfWords; i++) {
-				const randomNumOfWordsCount = asm.getRandomNumber(0, APP.notion.rssuabot.text.length - 1);
-				const randomWord = APP.notion.rssuabot.text[randomNumOfWordsCount];
-				message.push(randomWord);
+			const memberPressed = ctx.update.message?.reply_to_message?.from;
+			if (memberPressed) {
+				const memberPressedId = memberPressed.id;
+				const memberPressedfirstName = memberPressed.first_name;
+				const user = `<a href="tg://user?id=${memberPressedId}">${memberPressedfirstName}</a>`;
+
+				let message = [];
+
+				const countOfWords = asm.getRandomNumber(1, 10);
+				log(APP.notion.rssuabot.text);
+				for (let i = 0; i < countOfWords; i++) {
+					const randomNumOfWordsCount = asm.getRandomNumber(0, APP.notion.rssuabot.text.length - 1);
+					const randomWord = APP.notion.rssuabot.text[randomNumOfWordsCount];
+					message.push(randomWord);
+				}
+				const randomMsg = await ctx.replyWithHTML(`${user}, ${message.join(' ')}`);
+
+
+				setTimeout( async () => { // remove messages
+					try {
+						await ctx.deleteMessage(randomMsg.message_id);
+					} catch (error) { log(`ASM: Maybe message was removed by the user\n${error}`); }
+				}, asm.minToMs(60));
+			} else {
+				const msg = await ctx.replyWithHTML(`Команда /rp працює тільки як Reply!`);
+				setTimeout( async () => {
+					try {
+						await ctx.deleteMessage(msg.message_id);
+					} catch (error) { console.error('---------\n→ ASM ERR\n↓ ↓ ↓ ↓ ↓\n', error); }
+				}, asm.secToMs(5));
 			}
-			const randomMsg = await ctx.replyWithHTML(`${user}, ${message.join(' ')}`);
-
-			setTimeout( async () => { // remove messages
-				try {
-					await ctx.deleteMessage(randomMsg.message_id);
-				} catch (error) { log(`ASM: Maybe message was removed by the user\n${error}`); }
-			}, asm.secToMs(360000));
 		} catch (error) { console.error('---------\n→ ASM ERR\n↓ ↓ ↓ ↓ ↓\n', error);}
 	});
 
