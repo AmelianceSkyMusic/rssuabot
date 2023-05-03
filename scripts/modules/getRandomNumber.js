a0c2cc636ab276411a581f1c3a15cddd1732476c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-03 23:43:31 +0300

    ^ remove old bot and init bot on grammy

--- scripts/modules/getRandomNumber.js
+++
@@ -1,9 +0,0 @@
-// >----------------------------------------------------------------<
-// >                           FUNCTIONS                            <
-// >----------------------------------------------------------------<
-
-export default function getRandomNumber(min=0, max=9) {
-	if (min >= max) return undefined;
-	const randomNumber = Math.trunc(Math.random()*(max-min+1)) + min;
-	return randomNumber;
-}
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as dotenv from 'dotenv';
+import { Bot } from 'grammy';
+
+import { getRandomNumber } from '~/ameliance-scripts';
+import { returnError } from '~helpers/returnError';
+
+import { emoji } from './app/data/emoji';
+
+dotenv.config();
+
+const { BOT_TOKEN } = process.env;
+if (!BOT_TOKEN) throw new Error('BOT_TOKEN is not defined');
+
+const bot = new Bot(BOT_TOKEN);
+
+bot.on(':sticker', async (ctx) => {
+	const randomEmojiNumber = getRandomNumber(0, emoji.length - 1);
+	const randomEmoji = emoji[randomEmojiNumber];
+	try {
+		await ctx.reply(randomEmoji, { reply_to_message_id: ctx.msg.message_id });
+	} catch (error) { returnError(error); }
+});
+
+bot.start();
