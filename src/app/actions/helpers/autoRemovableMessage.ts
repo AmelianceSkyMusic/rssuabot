7a8d498b31facfff95ea805d351c2814dddcf3d7
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-04 18:46:50 +0300

    * add reply option to autoRemovableMessage helper

--- src/app/actions/helpers/autoRemovableMessage.ts
+++ src/app/actions/helpers/autoRemovableMessage.ts
@@ -2,20 +2,52 @@ import type { CommandContext, Context } from 'grammy';
 
 import { bot } from '~/index';
 
+import { generateFullName } from './generateFullName';
+import { generateUserTag } from './generateUserTag';
 import removeMsgById from './removeMessageById';
 
-export async function autoRemovableMessage(
-	ctx: CommandContext<Context>,
-	text: string,
-	ms = 3600,
-) {
-	const chatId = ctx.chat.id;
+interface AutoRemovableMessage {
+	ctx: CommandContext<Context>;
+	text: string;
+	reply?: boolean;
+	ms?: number;
+}
 
-	const sendMessage = await bot.api.sendMessage(
-		chatId,
-		text,
-		{ parse_mode: 'HTML' },
+export async function autoRemovableMessage({
+	ctx,
+	text,
+	reply = false,
+	ms = 3600,
+}: AutoRemovableMessage) {
+	const chatId = ctx.chat.id;
+	const messageId = ctx.msg.message_id;
+	const messageUserId = ctx.msg.from?.id;
+	const messageUserFirstName = ctx.msg.from?.first_name || '';
+	const messageUserLastName = ctx.msg.from?.last_name || '';
+	const messageUserFullNameName = generateFullName(
+		messageUserFirstName,
+		messageUserLastName,
 	);
 
-	await removeMsgById(ctx, sendMessage.message_id, ms);
+	const messageUserTag = generateUserTag(messageUserId || '', messageUserFullNameName);
+
+	let sendMessage = null;
+
+	if (reply) {
+		await ctx.reply(
+			`${messageUserTag}, ${text}`,
+			{
+				reply_to_message_id: messageId,
+				parse_mode: 'HTML',
+			},
+		);
+	} else {
+		sendMessage = await bot.api.sendMessage(
+			chatId,
+			text,
+			{ parse_mode: 'HTML' },
+		);
+	}
+
+	if (sendMessage) await removeMsgById(ctx, sendMessage.message_id, ms);
 }
