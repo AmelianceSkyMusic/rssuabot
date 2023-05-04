947fbd5d3fba52536bf6a33d6931ac7278bdac74
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-04 18:46:27 +0300

    = add generateUserTag helper

---
+++ src/app/actions/helpers/generateFullName.ts
@@ -0,0 +1,5 @@
+import { joinWith } from '~/ameliance-scripts/scripts';
+
+export function generateFullName(userFirstName: string, userLastName: string): string {
+	return joinWith(' ', [userFirstName, userLastName]);
+}
