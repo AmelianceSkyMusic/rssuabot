1c483fcb3f26f46ef7f2b2d8807cb811f3370c45
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-04 18:57:19 +0300

    * fix spelling filename mistake

--- src/app/actions/helpers/generateUserTag.ts
+++ src/app/actions/helpers/generateUserTag.ts
@@ -1,4 +1,4 @@
-import type { UserId } from '~types/user';
+import type { UserId } from '~types/User';
 
 export function generateUserTag(userId: UserId, userName: string): string {
 	return `<a href="tg://user?id=${userId}">${userName}</a>`;
