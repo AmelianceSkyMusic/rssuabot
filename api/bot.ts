25f7d6568ecf5a14cca3f36043f1075d67c35273
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-05 16:50:52 +0300

    * init fix deploy

--- api/bot.ts
+++ api/bot.ts
@@ -1,12 +1,8 @@
 import { Bot, webhookCallback } from 'grammy';
 
-const bot = new Bot(process.env.BOT_TOKEN || '');
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error('BOT_TOKEN is unset');
 
-bot.on('message', async (ctx) => {
-	await ctx.reply(ctx.msg.text || '');
-});
+const bot = new Bot(token);
 
-export default async (req: any, res: any) => {
-	await bot.api.setWebhook(process.env.WEBHOOK || '');
-	webhookCallback(bot, 'http')(req, res);
-};
+export default webhookCallback(bot, 'http');
