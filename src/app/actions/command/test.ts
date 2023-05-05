8a2b8614f21cc8ee4b464d4ace4a28afd4717ba5
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-06 00:07:36 +0300

    * try path

--- src/app/actions/command/test.ts
+++ src/app/actions/command/test.ts
@@ -1,9 +1,11 @@
+import { returnError } from '~src/ameliance-scripts/scripts';
+
 import { bot } from '../../../../api/bot';
 
 export function test() {
 	bot.command('test', async (ctx) => {
 		try {
 			await ctx.reply('test3', { reply_to_message_id: ctx.msg.message_id });
-		} catch (error) { console.log(error); }
+		} catch (error) { returnError(error); }
 	});
 }
