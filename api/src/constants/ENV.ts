25d5971ae1f108a5fbdb1a3fee0bd08974d8547d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-05 17:27:02 +0300

    * fix ENV

--- api/src/constants/ENV.ts
+++ api/src/constants/ENV.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as dotenv from 'dotenv';
 
+const ENVIRONMENT = process.env;
+
 dotenv.config();
 
-export const ENV = process.env;
+export const ENV = process.env.MODE === 'dev' ? process.env : ENVIRONMENT;
