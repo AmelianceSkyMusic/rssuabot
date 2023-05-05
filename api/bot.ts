ef26ebde9496c74a4e7fe1ab60075050c7bdd041
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-05 12:22:34 +0300

    * fix deploy

--- api/bot.ts
+++ api/bot.ts
@@ -9,30 +9,30 @@ if (!BOT_TOKEN) throw new Error('BOT_TOKEN is not defined');
 
 export const bot = new Bot(BOT_TOKEN);
 
-phrasesStore.getState().fetchStudentPhrasesData();
-phrasesStore.getState().fetchRandomPhrasesData();
-phrasesStore.getState().fetchRandomClickPhrasesData();
-phrasesStore.getState().fetchChatRepliesData();
+// phrasesStore.getState().fetchStudentPhrasesData();
+// phrasesStore.getState().fetchRandomPhrasesData();
+// phrasesStore.getState().fetchRandomClickPhrasesData();
+// phrasesStore.getState().fetchChatRepliesData();
 
 actions.on.sticker();
 actions.command.help();
-actions.command.app();
-actions.command.registration();
-actions.command.aboutjsfecourse();
-actions.command.roadmap();
-actions.command.docs();
-actions.command.dismission();
-actions.command.link();
-actions.command.codeofconduct();
-actions.command.stickers();
-actions.command.topic();
-actions.command.transliterate();
+// actions.command.app();
+// actions.command.registration();
+// actions.command.aboutjsfecourse();
+// actions.command.roadmap();
+// actions.command.docs();
+// actions.command.dismission();
+// actions.command.link();
+// actions.command.codeofconduct();
+// actions.command.stickers();
+// actions.command.topic();
+// actions.command.transliterate();
 
-// hidden commands
-actions.command.asm();
-actions.command.two();
-actions.command.test();
-actions.command.hidden();
+// // hidden commands
+// actions.command.asm();
+// actions.command.two();
+// actions.command.test();
+// actions.command.hidden();
 
 bot.start();
 
