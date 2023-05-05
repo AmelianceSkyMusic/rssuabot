7edf75ddbc4df187d1739671bcc4d50bca8e3b3b
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-05 16:55:06 +0300

    * add base commands

--- api/bot.ts
+++ api/bot.ts
@@ -5,4 +5,7 @@ if (!token) throw new Error('BOT_TOKEN is unset');
 
 const bot = new Bot(token);
 
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
+bot.on('message', (ctx) => ctx.reply('Got another message!'));
+
 export default webhookCallback(bot, 'http');
