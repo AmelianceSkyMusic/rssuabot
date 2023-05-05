f9139b6e3852b0fb9049148f2335c32669a617f1
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-06 00:26:15 +0300

    * add test fetch

--- src/app/actions/command/test.ts
+++ src/app/actions/command/test.ts
@@ -1,10 +1,27 @@
 import { bot } from '../../../../api/bot';
 import { returnError } from '../../helpers/returnError';
+import { phrasesStore } from '../../store/phrasesStore';
 
 export function test() {
 	bot.command('test', async (ctx) => {
 		try {
-			await ctx.reply('test5', { reply_to_message_id: ctx.msg.message_id });
+			await ctx.reply('test', { reply_to_message_id: ctx.msg.message_id });
+			const {
+				studentPhrases,
+				randomPhrases,
+				randomClickPhrases,
+				chatReplies,
+			} = phrasesStore.getState();
+			console.log('studentPhrases: ', studentPhrases);
+			console.log('randomPhrases: ', randomPhrases);
+			console.log('randomClickPhrases: ', randomClickPhrases);
+			console.log('chatReplies: ', chatReplies);
+			await ctx.reply([
+				studentPhrases.join(', '),
+				randomPhrases.join(', '),
+				randomClickPhrases.join(', '),
+				chatReplies.join(', '),
+			].join('\n'), { reply_to_message_id: ctx.msg.message_id });
 		} catch (error) { returnError(error); }
 	});
 }
