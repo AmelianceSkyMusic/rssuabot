db84d363152fec4b79bebfdce3f2f6267cccc3bf
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-05 15:22:27 +0300

    * try to fix deploy

--- api/bot.ts
+++ api/bot.ts
@@ -1,27 +1,9 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as dotenv from 'dotenv';
 import { Bot, webhookCallback } from 'grammy';
 
-dotenv.config();
+const bot = new Bot(process.env.BOT_TOKEN || '');
 
-export const ENV = process.env;
-
-const { BOT_TOKEN, HOST_URL, MODE } = ENV;
-if (!BOT_TOKEN) throw new Error('BOT_TOKEN is not defined');
-
-const bot = new Bot(BOT_TOKEN);
-
-bot.on(':sticker', async (ctx) => {
-	try {
-		console.log('randomEmoji: ');
-		await ctx.reply('randomEmoji', { reply_to_message_id: ctx.msg.message_id });
-	} catch (error) { console.log(error); }
+bot.on('message', async (ctx) => {
+	await ctx.reply('Hi there!');
 });
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
-if (MODE === 'production') bot.start();
-
-console.log('start bot');
-if (!HOST_URL) throw new Error('HOST_URL is not defined');
-export default webhookCallback(bot, HOST_URL);
+export default webhookCallback(bot, 'http');
