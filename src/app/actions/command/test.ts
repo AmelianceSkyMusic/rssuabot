d0fb2b1cc5f718e591a28c7f4de36e1471e9a720
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-06 10:16:44 +0300

    * add test logs

--- src/app/actions/command/test.ts
+++ src/app/actions/command/test.ts
@@ -5,22 +5,22 @@ export function test() {
 	bot.command('test', async (ctx) => {
 		try {
 			await ctx.reply('test', { reply_to_message_id: ctx.msg.message_id });
-			// const {
-			// 	studentPhrases,
-			// 	randomPhrases,
-			// 	randomClickPhrases,
-			// 	chatReplies,
-			// } = phrasesStore.getState();
-			// console.log('studentPhrases: ', studentPhrases);
-			// console.log('randomPhrases: ', randomPhrases);
-			// console.log('randomClickPhrases: ', randomClickPhrases);
-			// console.log('chatReplies: ', chatReplies);
-			// await ctx.reply([
-			// 	studentPhrases.join(', '),
-			// 	randomPhrases.join(', '),
-			// 	randomClickPhrases.join(', '),
-			// 	chatReplies.join(', '),
-			// ].join('\n'), { reply_to_message_id: ctx.msg.message_id });
+			const {
+				studentPhrases,
+				randomPhrases,
+				randomClickPhrases,
+				chatReplies,
+			} = phrasesStore.getState();
+			console.log('studentPhrases: ', studentPhrases);
+			console.log('randomPhrases: ', randomPhrases);
+			console.log('randomClickPhrases: ', randomClickPhrases);
+			console.log('chatReplies: ', chatReplies);
+			await ctx.reply([
+				studentPhrases.join(', '),
+				randomPhrases.join(', '),
+				randomClickPhrases.join(', '),
+				chatReplies.join(', '),
+			].join('\n'), { reply_to_message_id: ctx.msg.message_id });
 		} catch (error) { returnError(error); }
 	});
 }
