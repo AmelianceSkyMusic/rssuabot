43e4c4c7b134da54d73da1fe763d6ff7efeb40a1
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-06 10:25:54 +0300

    * backward test

--- src/app/actions/command/test.ts
+++ src/app/actions/command/test.ts
@@ -8,19 +8,19 @@ export function test() {
 			await ctx.reply('test', { reply_to_message_id: ctx.msg.message_id });
 			const {
 				studentPhrases,
-				randomPhrases,
-				randomClickPhrases,
-				chatReplies,
+				// randomPhrases,
+				// randomClickPhrases,
+				// chatReplies,
 			} = phrasesStore.getState();
 			console.log('studentPhrases: ', studentPhrases);
-			console.log('randomPhrases: ', randomPhrases);
-			console.log('randomClickPhrases: ', randomClickPhrases);
-			console.log('chatReplies: ', chatReplies);
+			// console.log('randomPhrases: ', randomPhrases);
+			// console.log('randomClickPhrases: ', randomClickPhrases);
+			// console.log('chatReplies: ', chatReplies);
 			await ctx.reply([
 				studentPhrases.join(', '),
-				randomPhrases.join(', '),
-				randomClickPhrases.join(', '),
-				chatReplies.join(', '),
+				// randomPhrases.join(', '),
+				// randomClickPhrases.join(', '),
+				// chatReplies.join(', '),
 			].join('\n'), { reply_to_message_id: ctx.msg.message_id });
 		} catch (error) { returnError(error); }
 	});
